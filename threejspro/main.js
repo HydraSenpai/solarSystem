3b86203e1507b0bc9dbea8232817fedcfd96c90f
nicholas <nicholas.daly.2020@strath.ac.uk>
2022-09-19 16:12:07 +0100

    created lighting and torus

--- threejspro/main.js
+++ threejspro/main.js
@@ -1,1 +1,69 @@
 import './style.css';
+
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+//setup
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
+
+//objects
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+//light
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+//const ambientLight = new THREE.AmbientLight(0xffffff);
+//scene.add(ambientLight);
+
+// Background
+const spaceTexture = new THREE.TextureLoader().load('images/galaxy.jpg');
+scene.background = spaceTexture;
+
+
+
+//camera
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+document.body.onscroll = moveCamera;
+moveCamera();
+
+//animation loop
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  //controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
